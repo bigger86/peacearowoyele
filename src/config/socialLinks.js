48cd90b85b5cb1df4a2f9452e116a7acdcc21e44
Jarowoyele <biggathanlyf@gmail.com>
2026-05-24 03:16:50 +0100

    Update social media links to Instagram, TikTok, and X (Twitter)

---
+++ src/config/socialLinks.js
@@ -0,0 +1,6 @@
+// Social Media Links Configuration
+export const socialLinks = {
+  instagram: 'https://www.instagram.com/designaspro?igsh=MTI2MDhzOWFtaXZhZQ==',
+  tiktok: 'https://www.tiktok.com/@designaspro?_r=1&_t=ZS-96Tr79krPcWh',
+  twitter: 'https://x.com/designaspro'
+};
